3fbb1f8ecf4278017b9ba5222ee9c4f346eefbe0
MahmudaDisa <dishamirza.h@gmail.com>
2022-10-24 23:03:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('server running')
+});
+
+app.listen(port, () => {
+    console.log(`surver is running, ${port}`)
+})
